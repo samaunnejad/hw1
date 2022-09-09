48cb93fce6b39c241527ac23307d5616e645a55d
Samaun Nejad <snejad@usc.edu>
2022-09-09 06:51:40 +0000

    Code changes for problem 1

--- split.cpp
+++ split.cpp
@@ -11,16 +11,63 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+
+using namespace std; 
 
 /* Add a prototype for a helper function here if you need */
+
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
 
+  if(in == NULL){
+
+    return;
+  }
+//Checks if value in node is an even & then passes it into evens Node list
+  else if(in->value % 2 == 0){
+    evens->next = in; 
+    evens = evens->next;
+    evens->next = NULL;  
+  }
+//Checks if value in node is an odd & then passes it into odds Node list
+  else {
+    odds->next = in; 
+    odds = odds->next; 
+    odds->next = NULL; 
+  }
+
+//recursive function that calls itself to split Linked List
+   split(in->next, odds, evens); 
 }
 
 /* If you needed a helper function, write it here */
 
 // WRITE YOUR CODE HERE
+
+void append(Node* in, int value) 
+{
+  if(in == NULL){
+      return;
+  }
+
+  Node* newNode = new Node(value, in);
+  in = newNode;
+
+  cout << in->value << " ";
+
+}
+
+void printList(Node* in) 
+{
+  if(in == NULL){
+    return;
+  }
+  cout << in->value << endl;
+  printList(in->next); 
+
+}
